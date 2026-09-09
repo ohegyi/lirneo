de5d8f08c5c242f9e0a198cdcd7ac39da0ec004a
ohegyi <148001757+ohegyi@users.noreply.github.com>
2026-09-09 07:59:04 -0400

    homepage update7

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import vitePrerender from 'vite-plugin-prerender' 
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    
+    vitePrerender({ staticDir: path.join(__dirname, 'dist'),routes:['/']})
   ],
   resolve: {
     dedupe: ['react', 'react-dom'],
