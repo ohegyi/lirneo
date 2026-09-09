cab896c1393dbb7c0a8da307d692dfd5787c0042
ohegyi <148001757+ohegyi@users.noreply.github.com>
2026-09-09 08:04:32 -0400

    homepage update9

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import prerender from 'vite-prerender-plugin'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    
-    prerender({ renderTarget: '#root', prerenderScript: './prerender.js', }),
   ],
   resolve: {
     dedupe: ['react', 'react-dom'],
