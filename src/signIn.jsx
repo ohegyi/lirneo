fae86717c0fce6e564a919ca8b7382d5f037f633
ohegyi <148001757+ohegyi@users.noreply.github.com>
2026-09-08 22:18:43 -0400

    homepage update2

--- src/signIn.jsx
+++ src/signIn.jsx
@@ -67,8 +67,7 @@ style={{position: 'relative'}}
 </div>
     </div>
     <Card style ={{backgroundColor:'#eaeaeaff'}} >
-      <Text>lirneo is a tutor management platform for schools and tutoring programs. It streamlines requesting, assigning, and managing tutoring sessions for administrators, tutors, students, and their teachers. By adding administrative oversight to all tutoring, lirneo enables schools to better serve their students.</Text>
-      <div style={{display:'flex',gap:'30px',justifyContent:'center', paddingTop:'20px',paddingTop:'20px'}}>
+<Text>lirneo is a tutor management platform for schools and tutoring programs. It helps administrators request, assign, schedule, and manage tutoring sessions while coordinating information for tutors, students, and teachers. lirneo gives schools administrative oversight of their tutoring programs, making it easier to organize tutoring and support students' learning needs.</Text>      <div style={{display:'flex',gap:'30px',justifyContent:'center', paddingTop:'20px',paddingTop:'20px'}}>
         <Text component="a" href="https://www.lirneo.com/privacy.html" td="underline" c='dimmed'>Privacy Policy</Text>
         <Text component="a" href="https://www.lirneo.com/terms.html" td="underline" c='dimmed'>Terms of Use</Text>
       </div>
